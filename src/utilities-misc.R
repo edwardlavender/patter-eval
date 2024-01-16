54deb75c073979f3f4b4bb9f9b5a203784e47f90
edwardlavender <el72@st-andrews.ac.uk>
2024-01-16 15:46:53 +0100

    Add secs function

--- src/utilities-misc.R
+++ src/utilities-misc.R
@@ -1,5 +1,9 @@
 #' @title Utilities
 
+secs <- function(time) {
+  as.numeric(lubridate::duration(time))
+}
+
 mins <- function(time1, time2) {
   as.numeric(difftime(time1, time2, units = "mins"))
 }
