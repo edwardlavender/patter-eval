0e3039f38fe9a99f433d76cfcccfcf063c25eff0
edwardlavender <el72@st-andrews.ac.uk>
2024-01-17 17:21:42 +0100

    Implement calculate-skill.R for performance simulations

--- R/008-calculate-skill.R
+++ R/008-calculate-skill.R
@@ -8,8 +8,6 @@
 #### Prerequisites
 # 1) Analyse simulated data using selected algorithms
 
-#### Time records
-# *
 
 #########################
 #########################
@@ -23,10 +21,16 @@ options(error = function(...) beepr::beep(7))
 
 #### Essential packages
 library(dv)
+library(patter)
 library(data.table)
 library(dtplyr)
 library(dplyr, warn.conflicts = FALSE)
+library(tictoc)
 dv::src()
+
+#### Load data
+# sims <- readRDS(here_input("sims.rds"))
+sims <- readRDS(here_input("sims-performance.rds"))
 
 
 #########################
@@ -38,25 +42,41 @@ dv::src()
 # * Read path UD & algorithm UD (if available)
 # * Calculate skill for relevant algorithms
 
-skills <- pbapply::pblapply(split(sims, seq_len(nrow(sims))), function(sim) {
+# ~ 3.7 mins, 10 cl, 600 performance simulations
+
+tic()
+skills <- cl_lapply(.x = split(sims, seq_len(nrow(sims))),
+                    .cl = 10L,
+                    .chunk = TRUE,
+                    .fun = function(sim) {
+
+  #### Define outfile
+  out_file <- here_output("skill", paste0(sim$id, ".rds"))
+  if (file.exists(out_file)) {
+    return(readRDS(out_file))
+  }
 
   #### Read UDs
+  # sim <- sims[1, ]
+  print(sim$row)
   path   <- terra::rast(here_alg(sim, "path", "ud.tif"))
-  #
-  # TO DO (add control, a blank)
-  #
+  null   <- terra::rast(here_input("blank.tif"))
   if (sim$performance) {
     coa_1  <- read_rast(here_alg(sim, "coa", "30 mins", "ud.tif"))
     coa_2  <- read_rast(here_alg(sim, "coa", "120 mins", "ud.tif"))
+    # TO DO
+    # * Add RSP
   }
-  acpf   <- read_rast(here_alg(sim, "patter", "acpf", sim$alg_par, "ud.tif"))
-  acdcpf <- read_rast(here_alg(sim, "patter", "acdcpf", sim$alg_par, "ud.tif"))
+  acpfk   <- read_rast(here_alg(sim, "patter", "acpf", sim$alg_par, "ud-k.tif"))
+  acdcpfk <- read_rast(here_alg(sim, "patter", "acdcpf", sim$alg_par, "ud-k.tif"))
+  # TO DO
+  # * Add backward sampler results
 
   #### Define blank skill table
   skill <- data.table(
     id = sim$id,
     performance = sim$performance,
-    alg = c("COA(30)", "COA(120)", "ACPF", "ACDCPF"),
+    alg = c("Null", "COA(30)", "COA(120)", "ACPF(K)", "ACDCPF(K)"),
     mb = NA_real_,
     me = NA_real_,
     rmse = NA_real_,
@@ -67,7 +87,7 @@ skills <- pbapply::pblapply(split(sims, seq_len(nrow(sims))), function(sim) {
   #### Calculate skill metrics
   if (sim$performance) {
     # We will calculate model skill scores for each algorithm
-    uds <- list(coa_1, coa_2, acpf, acdcpf)
+    uds <- list(null, coa_1, coa_2, acpfk, acdcpfk)
   } else {
     # We will calculate model skill scores for patter algorithms only
     uds <- list(NULL, NULL, acpf, acdcpf)
@@ -79,10 +99,11 @@ skills <- pbapply::pblapply(split(sims, seq_len(nrow(sims))), function(sim) {
   skill$d    <- skill_by_alg(uds, path, skill_d)
 
   #### Return outputs
-  saveRDS(skill, here_output("skill", paste0(sim$id, ".rds")))
+  saveRDS(skill, out_file)
   skill
 
 }) |> rbindlist()
+toc()
 
 saveRDS(skills, here_data("sims", "synthesis", "skill-raw.rds"))
 
@@ -92,10 +113,12 @@ saveRDS(skills, here_data("sims", "synthesis", "skill-raw.rds"))
 #### Process skill scores
 
 #### Drop simulations which failed for all algorithms
-# skills <- skills[rowSums(!is.na(skills[, c("mb", "me", "rmse", "R", "d")])) > 0, ]
+pos_success <- rowSums(!is.na(skills[, c("mb", "me", "rmse", "R", "d")])) > 0
+skills[!pos_success, ]
+skills <- skills[pos_success, ]
 
 #### Add required information
-# merge with sims()
+skills <- merge(skills, sims, by = "id")
 
 #### Save processed data
 saveRDS(skills, here_data("sims", "synthesis", "skill.rds"))
