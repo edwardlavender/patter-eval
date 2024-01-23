86f74aa52f16a056a15d72320d288f68b8d01414
edwardlavender <el72@st-andrews.ac.uk>
2024-01-23 22:00:37 +0100

    In progress development of pf_backward_sampler_p()

--- src/patter-algorithms.R
+++ src/patter-algorithms.R
@@ -1,97 +1,141 @@
-pf_backward_sampler <- function(.history,
-                                .dens_step = dstep, ...,
-                                .save_history = FALSE, .write_history = NULL,
-                                .cl = NULL, .cl_varlist = NULL, .cl_chunks = TRUE,
-                                .verbose = TRUE
+#' @title Internal {patter} functions
+# utils.add::load_internal_functions("patter")
+check_dlist         <- patter:::check_dlist
+cat_init            <- patter:::cat_init
+call_start          <- patter:::call_start
+call_end            <- patter:::call_end
+call_time           <- patter:::call_time
+.pf_history_list    <- patter:::.pf_history_list
+.pf_history_read    <- patter:::.pf_history_read
+.pf_history_cols    <- patter:::.pf_history_cols
+.pf_history_elm     <- patter:::.pf_history_elm
+.pf_history_write   <- patter:::.pf_history_write
+.pf_write_particles <- patter:::.pf_write_particles
+
+fnrow <- collapse::fnrow
+
+#' @title Proposal densities
+
+pf_dpropose_read <- function(.particles, .obs, .t, .dlist) {
+  # Check inputs
+  if (.t == max(.obs$timestep)) {
+    check_dlist(.dlist = .dlist,
+                .algorithm = "sim")
+  }
+  # Read precomputed densities from file
+  spat_dens <-
+    here_input("density",
+               .dlist$algorithm$sim$combination,
+               paste0(.particles$cell_now[1], ".qs")) |>
+    qs::qread()
+  # Match densities & return .particles with a 'dens' column
+  # * This behaviour matches pf_dpropose()
+  .particles[, dens := spat_dens[match(.particles$cell_past, spat_dens$cell)]]
+  .particles[dens > 0, ]
+}
+
+#' @title Backward sampler (parallel implementation)
+
+pf_backward_sampler_p <- function(.history,
+                                  .dpropose = pf_dpropose, ...,
+                                  .obs = NULL,
+                                  .dlist,
+                                  .cl = NULL,
+                                  .cl_varlist = NULL,
+                                  .cl_chunk = ifelse(is.null(.cl), FALSE, TRUE),
+                                  .record = pf_opt_record(),
+                                  .verbose = getOption("patter.verbose")
 ) {
 
   #### Check user inputs
-  t_onset <- Sys.time()
-  check_inherits(.history, "list")
-  if (!.save_history && is.null(.write_history)) {
-    abort("`.save_history = FALSE` and `.write_history = NULL`. There is nothing to do.")
-  }
-  write_history_folder <- .pf_check_write_history(.write_history)
+  # TO DO
 
-  #### Set up messages (as usual)
+  #### Set up messages
+  t_onset <- Sys.time()
   cat_log <- cat_init(.verbose = .verbose)
-  cat_log(call_start(.fun = "pf_backward_sampler", .start = t_onset))
-  on.exit(cat_log(call_end(.fun = "pf_backward_sampler", .start = t_onset, .end = Sys.time())), add = TRUE)
+  cat_log(call_start(.fun = "pf_backward_sampler_p", .start = t_onset))
+  on.exit(cat_log(call_end(.fun = "pf_backward_sampler_p", .start = t_onset,
+                           .end = Sys.time())), add = TRUE)
+  .history <- .pf_history_list(.history)
+  read     <- .pf_history_read(.history)
 
   #### Set up loop
-  cat_log("... Set up...")
-  # Define whether or not to read history files
-  if (inherits(.history[[1]], "data.frame")) {
-    read_history <- FALSE
-  } else {
-    read_history <- TRUE
-  }
-  # Define constants
-  n_step     <- length(.history)
-  if (read_history) {
-    .history[[n_step]] <- arrow::read_parquet(.history[[n_step]])
-  }
+  # .history
+  .history  <- .pf_history_list(.history)
+  read      <- .pf_history_read(.history)
+  inout     <- .pf_history_cols(.history = .history, .record = .record,
+                                .input_cols = c("cell_now", "x_now", "y_now"))
+  .record   <- inout$.record
+  read_cols <- inout$read_cols
+  write     <- .pf_history_write(.record)
+  # Final (starting) particle samples for the backward sampler
+  n_step <- length(.history)
+  .history[[n_step]] <- .pf_history_elm(.history = .history, .elm = n_step,
+                                        cols = read_cols)
   n_particle <- fnrow(.history[[n_step]])
-  density    <- NULL
+  # Global variables
+  dens <- NULL
 
   #### Generate paths
   cat_log("... Generating path(s)...")
   paths <-
-    cl_lapply(seq_len(n_particle),
-              .cl = .cl,
-              .varlist = .cl_varlist,
-              .use_chunks = .cl_chunks,
-              .fun = function(i) {
+    cl_lapply(
+      seq_len(n_particle),
+      .cl = .cl,
+      .varlist = .cl_varlist,
+      .chunk = .cl_chunk,
+      .fun = function(i) {
 
-                #### Set up loop
-                cat_log(paste("... ... On particle", i, "..."))
-                cat_log("... ... ... Preparing to run sampler...")
-                path <- dens <- list()
-                path[[n_step]] <- .history[[n_step]][i, ]
-                path[[n_step]][, density := 1]
+        #### Set up loop
+        cat_log(paste("... ... On particle", i, "..."))
+        cat_log("... ... ... Preparing to run sampler...")
+        path <- density <- list()
+        path[[n_step]] <- .history[[n_step]][i, ]
+        path[[n_step]][, dens := 1]
 
-                #### Run backwards sampler for a selected particle (i)
-                cat_log("... ... ... Running sampler...")
-                for (t in n_step:2) {
-                  # Read history if necessary
-                  cat_log(paste("... ... ... ... On time step", t, "..."))
-                  if (read_history) {
-                    # Drop history for t (to save memory) & read new history
-                    .history[[t]] <- NA
-                    .history[[t - 1]] <- arrow::read_parquet(.history[[t - 1]])
-                  }
-                  # Calculate step densities
-                  dens <- .dens_step(.data_now = path[[t]],
-                                     .data_past = .history[[t - 1]], ...)
-                  # Sample a previous location
-                  index <- sample.int(fnrow(.history[[t - 1]]), size = 1, prob = dens)
-                  path[[t - 1]] <- .history[[t - 1]][index, ]
-                  path[[t - 1]][, density := dens[index]]
-                }
+        #### Run backwards sampler for a selected particle (i)
+        cat_log("... ... ... Running sampler...")
+        for (t in n_step:2) {
+          # Read history if necessary
+          cat_log(paste("... ... ... ... On time step", t, "..."))
+          tp <- t - 1L
+          if (read) {
+            # Drop history for t (to save memory) & read new history
+            .history[[t]] <- NA
+            .history[[tp]] <- .pf_history_elm(.history = .history, .elm = tp,
+                                              cols = read_cols)
+          }
+          # Calculate step densities
+          #
+          # FIX THIS
+          #
+          prob <- .dpropose(path[[t]], .history[[tp]], ...)$dens
+          # Sample a previous location
+          index <- sample.int(fnrow(.history[[tp]]), size = 1, prob = prob)
+          path[[tp]] <- .history[[tp]][index, ]
+          path[[tp]][, dens := prob[index]]
+        }
 
-                #### Collate path
-                cat_log("... ... ... Collating paths...")
-                path <-
-                  path |>
-                  rbindlist() |>
-                  mutate(path_id = i, .before = 1L) |>
-                  as.data.table()
+        #### Collate path
+        cat_log("... ... ... Collating paths...")
+        path <-
+          path |>
+          rbindlist() |>
+          mutate(path_id = i, .before = 1L) |>
+          as.data.table()
 
-                #### Save path
-                cat_log("... ... ... Recording path...")
-                if (!is.null(.write_history)) {
-                  .write_history$x    <- path
-                  .write_history$sink <- file.path(write_history_folder, paste0(t, ".parquet"))
-                  do.call(arrow::write_parquet, .write_history)
-                }
-                # Save path in memory
-                if (.save_history) {
-                  return(path)
-                } else {
-                  return(NULL)
-                }
+        #### Save path
+        cat_log("... ... ... Recording path...")
 
-              })
+        .pf_write_particles(.particles = path, .sink = .record$sink,
+                            .filename = t, .write = write)
+        # Save path in memory
+        if (!.record$save) {
+          return(path)
+        } else {
+          return(NULL)
+        }
+      })
 
   #### Return outputs
   cat_log("... Completing simulation...")
@@ -112,6 +156,6 @@ pf_backward_sampler <- function(.history,
               time = time)
 
   #### Return outputs
-  class(out) <- c(class(out), "pf_path")
+  # class(out) <- c(class(out), "pf_path")
   out
 }
