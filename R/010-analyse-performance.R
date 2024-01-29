f95f410786edee7bc30b1e4888db8e4776676493
edwardlavender <edlavender01@gmail.com>
2024-01-29 20:43:05 +0100

    Improve preformance plots

--- R/010-analyse-performance.R
+++ R/010-analyse-performance.R
@@ -55,117 +55,119 @@ metrics_lim <-
        d = c(0, 1))
 
 #### Define algorithms
-# Define algorithms (factor)
-unique(skills$alg)
-algs <- c("Null", "COA(30)", "COA(120)", "RSP(1)", "RSP(2)", "ACPF(F)", "ACDCPF(F)", "ACPF(K)", "ACDCPF(K)", "ACPF(S)", "ACDCPF(S)")
-skills$alg <- factor(skills$alg, levels = algs, labels = seq_len(length(algs)))
-# (optional) Subset algorithms
-# * This only affects skills plots
-# * Code for mapping requires manual adjustment (see FLAG statements)
-if (TRUE) {
-  algs_sbt <- c("Null", "COA(30)", "COA(120)", "RSP(1)", "RSP(2)", "ACPF(F)", "ACDCPF(F)", "ACPF(S)", "ACDCPF(S)")
-  algs_ind <- as.character(which(algs %in% algs_sbt))
-  skills <-
-    skills |>
-    filter(alg %in% algs_ind) |>
-    mutate(alg = factor(alg, levels = algs, labels = seq_len(length(algs)))) |>
-    as.data.table()
-}
+# Define algorithms (subset)
+algs <- data.frame(alg = c("Null", "COA(30)", "COA(120)", "RSP(1)", "RSP(2)", "ACPF(F)", "ACDCPF(F)", "ACPF(S)", "ACDCPF(S)"),
+                   col_name = c("dimgrey", "red", "darkred", "orange", "darkorange", "slateblue1", "slateblue4", "lightgreen", "darkgreen")
+                   )
+algs$col <- scales::alpha(algs$col_name, 0.5)
+algs$label <- seq_len(nrow(algs))
+skills <-
+  skills |>
+  filter(alg %in% algs$alg) |>
+  left_join(algs, by = "alg", relationship = "many-to-one") |>
+  as.data.table()
 
 
 #########################
 #########################
 #### Maps of space use
 
-#### Select simulations
-# Use specific combination, two receiver levels, one array/path realisation
-sims_for_maps <-
-  sims |>
-  filter(combination == 1L) |>
-  filter(n_receiver %in% nr) |>
-  filter(array_realisation == 1L & path_realisation == 1L) |>
-  arrange(arrangement, n_receiver) |>
-  as.data.table()
+# Make maps (~37 s)
+if (FALSE) {
 
-#### Set up plot
-# The figure will be annotated outside of R
-# (optional) FLAG: adjust width & number of columns for algorithms
-dir.create(here_fig("performance"), recursive = TRUE)
-png(here_fig("performance", png_name("map")),
-    height = 8, width = 12, units = "in", res = 600)
-pp <- par(mfrow = c(4, 9),
-          oma = c(0, 0, 0, 0), mar = rep(1, 4))
-pbapply::pblapply(1:4, function(i) {
+  #### Select simulations
+  # Use specific combination, two receiver levels, one array/path realisation
+  sims_for_maps <-
+    sims |>
+    filter(combination == 1L) |>
+    filter(n_receiver %in% nr) |>
+    filter(array_realisation == 1L & path_realisation == 1L) |>
+    arrange(arrangement, n_receiver) |>
+    as.data.table()
 
-  #### Read UDs
-  sim        <- sims_for_maps[i, ]
-  # list.files(here_alg(sim), recursive = TRUE)
-  ud_path    <- terra::rast(here_alg(sim, "path", "ud.tif"))
-  ud_coa_1   <- terra::rast(here_alg(sim, "coa", "30 mins", "ud.tif"))
-  ud_coa_2   <- terra::rast(here_alg(sim, "coa", "120 mins", "ud.tif"))
-  ud_rsp_1   <- terra::rast(here_alg(sim, "rsp", "default", "ud.tif"))
-  ud_rsp_2   <- terra::rast(here_alg(sim, "rsp", "custom", "ud.tif"))
-  ud_acpff   <- terra::rast(here_alg(sim, "patter", "acpf", sim$alg_par, "ud-f.tif"))
-  ud_acdcpff <- terra::rast(here_alg(sim, "patter", "acdcpf", sim$alg_par, "ud-f.tif"))
-  ud_acpfk   <- terra::rast(here_alg(sim, "patter", "acpf", sim$alg_par, "ud-k.tif"))
-  ud_acdcpfk <- terra::rast(here_alg(sim, "patter", "acdcpf", sim$alg_par, "ud-k.tif"))
-  ud_acpfs   <- terra::rast(here_alg(sim, "patter", "acpf", sim$alg_par, "ud-s.tif"))
-  ud_acdcpfs <- terra::rast(here_alg(sim, "patter", "acdcpf", sim$alg_par, "ud-s.tif"))
-  # Select UDs
-  # * (optional) FLAG: modify UDs included in this list
-  uds <- list(ud_path,
-              ud_coa_1, ud_coa_2,
-              ud_rsp_1, ud_rsp_2,
-              ud_acpff, ud_acdcpff,
-              # ud_acpfk, ud_acdcpfk,
-              ud_acpfs, ud_acdcpfs
-              )
+  #### Set up plot
+  # The figure will be annotated outside of R
+  # (optional) FLAG: adjust width & number of columns for algorithms
+  dir.create(here_fig("performance"), recursive = TRUE)
+  png(here_fig("performance", png_name("map")),
+      height = 8, width = 12, units = "in", res = 600)
+  pp <- par(mfrow = c(4, 9),
+            oma = c(0, 0, 0, 0), mar = rep(1, 4))
+  pbapply::pblapply(1:4, function(i) {
 
-  #### Calculate scaling parameter
-  # We will scale UDs (within rows) to a maximum value of 1 for comparison
-  # Scaling is implemented below to ensure correct HR calculation
-  scale <- sapply(uds, \(ud) terra::global(ud, "max")$max) |> max()
-  # uds   <- lapply(uds, \(ud) ud / scale)
-  # sapply(uds, \(ud) terra::global(ud, "max"))
+    #### Read UDs
+    sim        <- sims_for_maps[i, ]
+    # list.files(here_alg(sim), recursive = TRUE)
+    ud_path    <- terra::rast(here_alg(sim, "path", "ud.tif"))
+    ud_coa_1   <- terra::rast(here_alg(sim, "coa", "30 mins", "ud.tif"))
+    ud_coa_2   <- terra::rast(here_alg(sim, "coa", "120 mins", "ud.tif"))
+    ud_rsp_1   <- terra::rast(here_alg(sim, "rsp", "default", "ud.tif"))
+    ud_rsp_2   <- terra::rast(here_alg(sim, "rsp", "custom", "ud.tif"))
+    ud_acpff   <- terra::rast(here_alg(sim, "patter", "acpf", sim$alg_par, "ud-f.tif"))
+    ud_acdcpff <- terra::rast(here_alg(sim, "patter", "acdcpf", sim$alg_par, "ud-f.tif"))
+    ud_acpfk   <- terra::rast(here_alg(sim, "patter", "acpf", sim$alg_par, "ud-k.tif"))
+    ud_acdcpfk <- terra::rast(here_alg(sim, "patter", "acdcpf", sim$alg_par, "ud-k.tif"))
+    ud_acpfs   <- terra::rast(here_alg(sim, "patter", "acpf", sim$alg_par, "ud-s.tif"))
+    ud_acdcpfs <- terra::rast(here_alg(sim, "patter", "acdcpf", sim$alg_par, "ud-s.tif"))
+    # Select UDs
+    # * (optional) FLAG: modify UDs included in this list
+    uds <- list(ud_path,
+                ud_coa_1, ud_coa_2,
+                ud_rsp_1, ud_rsp_2,
+                ud_acpff, ud_acdcpff,
+                # ud_acpfk, ud_acdcpfk,
+                ud_acpfs, ud_acdcpfs
+    )
 
-  #### Plot UDs for selected array
-  m <- read_array(sim)
-  lapply(uds, function(ud) {
-    # Plot UD (scaled)
-    sud <- ud / scale
-    spatMap(sud, range = c(0, 1), legend = FALSE, mar = NA)
-    # (optional) Set speed to TRUE to check plot layout only
-    speed <- FALSE
-    if (speed) {
+    #### Calculate scaling parameter
+    # We will scale UDs (within rows) to a maximum value of 1 for comparison
+    # Scaling is implemented below to ensure correct HR calculation
+    scale <- sapply(uds, \(ud) terra::global(ud, "max")$max) |> max()
+    # uds   <- lapply(uds, \(ud) ud / scale)
+    # sapply(uds, \(ud) terra::global(ud, "max"))
+
+    #### Plot UDs for selected array
+    m <- read_array(sim)
+    lapply(uds, function(ud) {
+      # Plot UD (scaled)
+      sud <- ud / scale
+      spatMap(sud, range = c(0, 1), legend = FALSE, mar = NA)
+      # (optional) Set speed to TRUE to check plot layout only
+      speed <- FALSE
+      if (speed) {
+        spatAxes(ud)
+        return(NULL)
+      }
+      # Add home range (based on unscaled UD)
+      map_hr_home(ud, .add = TRUE,
+                  border = "dimgrey", lwd = 0.75)
+      # Plot UD (scaled)
+      # terra::plot(ud)
+      # Add receivers
+      points(m$receiver_easting, m$receiver_northing,
+             pch = 21,
+             bg = scales::alpha("black", 0.75),
+             col = scales::alpha("black", 0.75),
+             cex = 0.5)
+      # Add detection range(s)
+      # * We use a scale bar instead
+      if (FALSE) {
+        cbind(m$receiver_easting, m$receiver_northing) |>
+          terra::vect() |>
+          terra::buffer(width = sim$gamma) |>
+          terra::lines(lwd = 0.5, col = "dimgrey")
+      }
       spatAxes(ud)
-      return(NULL)
-    }
-    # Add home range (based on unscaled UD)
-    map_hr_home(ud, .add = TRUE,
-                border = "dimgrey", lwd = 0.75)
-    # Plot UD (scaled)
-    # terra::plot(ud)
-    # Add receivers
-    points(m$receiver_easting, m$receiver_northing,
-           pch = 21,
-           bg = scales::alpha("black", 0.75),
-           col = scales::alpha("black", 0.75),
-           cex = 0.5)
-    # Add detection range(s)
-    # * We use a scale bar instead
-    if (FALSE) {
-      cbind(m$receiver_easting, m$receiver_northing) |>
-        terra::vect() |>
-        terra::buffer(width = sim$gamma) |>
-        terra::lines(lwd = 0.5, col = "dimgrey")
-    }
-    spatAxes(ud)
-    terra::sbar(sim$gamma, lonlat = FALSE, col = "darkred")
-    NULL
-  }) |> invisible()
+      terra::sbar(sim$gamma, lonlat = FALSE, col = "darkred")
+      NULL
+    }) |> invisible()
 
-}) |> invisible()
-dev.off()
+  }) |> invisible()
+  dev.off()
+
+}
+
+
 
 
 #########################
@@ -187,7 +189,7 @@ sims_for_skill <-
 #### Visualise boxplots
 png(here_fig("performance", png_name("barplots")),
     height = 10, width = 15, units = "in", res = 600)
-pp <- par(mfrow = c(4, length(unique(skills$alg))),
+pp <- par(mfrow = c(4, length(metrics)),
           oma = c(1, 3, 1, 1), mar = c(2, 2, 2, 2))
 lapply(1:4, function(i) {
   # Define skill scores across path realisations
@@ -198,13 +200,15 @@ lapply(1:4, function(i) {
   # Visualise distribution of error scores (by metric)
   lapply(metrics, function(metric) {
     # metric <- metrics[1]
-    x <- skill$alg
+    x <- skill$label # skill$alg
     y <- skill[[metric]]
     # p <- min(floor(log10(abs(y))), na.rm = TRUE)
     # y <- y / 10^p
     pretty_boxplot(x, y,
                    varwidth = TRUE,
+                   col = skill$col,
                    ylim = metrics_lim[[metric]],
+                   pretty_axis_args = list(side = 1:2, pretty = list(list(n = 100), list(n = 5))),
                    las = TRUE,
                    xlab = "", ylab = "")
   }) |> invisible()
@@ -218,10 +222,17 @@ dev.off()
 
 # TO DO
 # * use n_receivers or detection coverage here
-#
 
+#### (optional) Subset algorithms for improved clarity on plot
+unique(skills$alg)
+skills_for_trends <-
+  skills |>
+  filter(alg %in% c("Null", "COA(30)", "RSP(1)", "ACPF(S)", "ACDCPF(S)")) |>
+  as.data.table()
 combs <- unique(sims$combination)
 nc    <- length(unique(sims$combination))
+
+
 png(here_fig("performance", "relationships.png"),
     height = 4 * nc, width = 12, units = "in", res = 600)
 pp <- par(mfrow = c(nc * 2, length(metrics)), oma = c(1, 3, 1, 1), mar = rep(2, 4))
@@ -229,7 +240,7 @@ lapply(combs, function(combination) {
 
   # Identify skill datasets
   # combination <- 1
-  skill <- skills[system_type == combination, ]
+  skill <- skills_for_trends[system_type == combination, ]
 
   # Loop over array arrangements
   lapply(split(skill, skill$arrangement), function(sk) {
@@ -240,13 +251,13 @@ lapply(combs, function(combination) {
       x <- sk$n_receiver + rnorm(nrow(sk), 0, 1)
       y <- sk[[metric]]
       pretty_plot(x, y,
-                  col = scales::alpha(sk$alg, 0.75),
+                  col = scales::alpha(sk$col, 0.25),
                   xlab = "", ylab = "",
                   cex = 0.5, lwd = 0.5)
       # Add trends for each algorithm
       lapply(split(sk, sk$alg, drop = TRUE), function(d) {
        # d <- split(sk, sk$alg)[[1]]
-        add_smoother(d$n_receiver, d[[metric]], col = d$alg, lwd = 2)
+        add_smoother(d$n_receiver, d[[metric]], col = d$col, lwd = 2)
       }) |> invisible()
     }) |> invisible()
 
@@ -255,12 +266,8 @@ lapply(combs, function(combination) {
 dev.off()
 
 # Check colours
-plot(c(0, 1), c(0, 1), type = "n")
-lines(c(0, 1), c(0.1, 0.1), col = unique(skills$alg)[1])
-lines(c(0, 1), c(0.2, 0.2), col = unique(skills$alg)[2])
-lines(c(0, 1), c(0.3, 0.3), col = unique(skills$alg)[3])
-lines(c(0, 1), c(0.4, 0.4), col = unique(skills$alg)[4])
-lines(c(0, 1), c(0.5, 0.5), col = unique(skills$alg)[5])
+algs
+
 
 
 #### End of code.
