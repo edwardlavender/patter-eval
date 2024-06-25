5adc22a6c0b30962569c195ddadeaa72cef3418f
edwardlavender <edward.lavender@eawag.ch>
2024-06-25 21:01:03 +0200

    Set options

--- src/options.R
+++ src/options.R
@@ -1,4 +1,13 @@
 # Progress bar (chunk) options for cl_lapply()
 pbapply::pboptions("nout" = 10)
 
+# Set options
+op <- options()
+# Set terra colour palette
 options(terra.pal = rev(terrain.colors(256)))
+# On lavended, beep on error
+if (Sys.info()["user"] == "lavended") {
+  options(error = function(...) beepr::beep(7))
+}
+
+
