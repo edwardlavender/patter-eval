fdc6e021e8b29e98ada7a30d9b3e026c764a178e
edwardlavender <edward.lavender@eawag.ch>
2024-06-26 08:59:36 +0200

    Update progress bar gradations

--- src/options.R
+++ src/options.R
@@ -1,7 +1,4 @@
-# Progress bar (chunk) options for cl_lapply()
-pbapply::pboptions("nout" = 10)
-
-# Set options
+# Set global options
 op <- options()
 # Set terra colour palette
 options(terra.pal = rev(terrain.colors(256)))
@@ -10,4 +7,9 @@ if (Sys.info()["user"] == "lavended") {
   options(error = function(...) beepr::beep(7))
 }
 
-
+# Progress bar (chunk) options for cl_lapply()
+if (Sys.info()["user"] == "lavended") {
+  pbapply::pboptions("nout" = 10)
+} else {
+  pbapply::pboptions("nout" = 50)
+}
